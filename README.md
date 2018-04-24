3d55825a79660552e684fcad63309840cfa81738
Marcin <marcin.rosik96@gmail.com>
2018-04-24 19:54:29 +0200

    Testing our STM32 and README changes
    Simple program, testing our device by turning on built-in LED. Its working. Also e

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # STParkSensors
-University Project. Main idea is to program uC as a ParkSensor. 
 
-Used software:
--System Workbench for STM32
+Overview - University Project. Main idea is to program uC as a ParkSensor. 
 
-Used hardware:
--STM32F407VGT
--HC-SR04 2-200cm
+Description - Used hardware: STM32F407VGT, HC-SR04 2-200cm.
+
+Tools - Used software is System Workbench for STM32, C language project.
+
+How to run - For now, just compile and run.
+
+How to compile - Default C language compilator for STM32 architecture.
+
+Future improvements -
+
+Attributions - 
+
+License -
+
+Credits - Marcin Rosik, Kamil Pomorski
+
+The project was conducted during the Microprocessor Lab course held by the Institute of Control and Information Engineering, Poznan University of Technology.
+Supervisor: Tomasz Mañkowski
--- src/main.c
+++ src/main.c
@@ -15,6 +15,28 @@
 
 int main(void)
 {
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
-	for(;;);
+	GPIO_InitTypeDef GPIOStruct;
+	GPIOStruct.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	GPIOStruct.GPIO_Mode = GPIO_Mode_OUT;
+	GPIOStruct.GPIO_OType = GPIO_OType_PP;
+	GPIOStruct.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIOStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+	GPIO_Init(GPIOD, &GPIOStruct);
+	int licznik=0;
+	for(;;)
+	{
+		if(licznik >4000000)licznik=0;
+		if(licznik > 0 && licznik <1000000-1) {GPIO_SetBits(GPIOD, GPIO_Pin_12);}
+		else if(licznik > 1000000 && licznik <2000000-1) {GPIO_SetBits(GPIOD, GPIO_Pin_13);}
+		else if(licznik > 2000000 && licznik <3000000-1) {GPIO_SetBits(GPIOD, GPIO_Pin_14);}
+		else  {GPIO_SetBits(GPIOD, GPIO_Pin_15);}
+		licznik++;
+		GPIO_ResetBits(GPIOD, GPIO_Pin_12);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_14);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_15);
+	}
 }
